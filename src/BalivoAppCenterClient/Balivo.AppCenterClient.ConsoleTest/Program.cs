81be77c6c312ea9d0f65ab5d43742577615bd6ea
Jefferson Balivo <jefferson@balivo.com.br>
2018-10-26 09:07:43 -0300

    Delete "src" content; New AppCenterClient library;

---
+++ src/BalivoAppCenterClient/Balivo.AppCenterClient.ConsoleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balivo.AppCenterClient.ConsoleTest
+{
+    class Program
+    {
+        static IAppCenterClient AppCenterClient;
+
+        static void Main(string[] args)
+        {
+            var token = "";
+
+            AppCenterClient = new AppCenterClient(new AppCenterCredentials(token));
+
+            Task.Factory.StartNew(async () =>
+            {
+
+                var pushResponse = await AppCenterClient.Push.SendAsync(new Models.NotificationDefinition { }, "", "");
+
+
+
+            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
